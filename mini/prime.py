9239b120b9f41ce12c150b223d1d67423091f8c8
James Cowgill <james410@cowgill.org.uk>
2012-10-20 01:36:54 +0100

    Fix code style (mainly whitespace changes)

--- mini/prime.py
+++ mini/prime.py
@@ -12,34 +12,36 @@ primes_file = open("primes", "a+")
 primes_file.seek(0)
 prime_list = [int(x) for x in primes_file.readlines()]
 
+
 def _prime_test_with_list(number):
     '''Tests just using the primes in the primes_list'''
-    
+
     # Calculate cutoff
     cutoff = int(math.sqrt(number))
-    
+
     # Find primes
     for prime in prime_list:
         # Exit if above the cutoff
         if prime > cutoff:
             break
-        
+
         if number % prime == 0:
             # number is NOT prime
             return False
-        
+
     return True
+
 
 def prime_test(number):
     '''Tests whether the given integer is a prime number'''
-    
+
     # Force number to integer
     number = int(number)
-    
+
     # Ignore numbers less than 2
     if number < 2:
         return False
-    
+
     # Find starting prime
     if len(prime_list) == 0:
         # Reset list with 2
@@ -49,14 +51,14 @@ def prime_test(number):
     else:
         # Start with the last prime in the file
         first_prime = prime_list[len(prime_list) - 1]
-        
+
     # Calculate all the primes we need
     for i in range(first_prime + 1, int(math.sqrt(number))):
         if _prime_test_with_list(i):
             # Add to list of primes
             prime_list.append(i)
             primes_file.write(str(i) + "\n")
-            
+
     # Finally, test the prime using just the primes list
     return _prime_test_with_list(number)
 
@@ -65,16 +67,16 @@ if __name__ == '__main__':
     # Get number to test
     print("Primality Tester - James Cowgill")
     print(" Enter a blank line to exit")
-    
+
     # Find some primes!
     while True:
         print()
         in_str = input("Enter number to test: ")
-        
+
         # Empty?
         if len(in_str) == 0:
             break
-        
+
         # Test and print result
         if prime_test(in_str):
             print(str(int(in_str)) + " is prime")

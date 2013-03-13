30191441bf6b5bd6723af3e6ac2e07b8bd5b0ad0
James Cowgill <james410@cowgill.org.uk>
2013-03-13 12:31:40 +0000

    [Prac 16] Fixed tabs in lillian's set code

--- java/src/jcowgill/prac16/sets/LinkedSetDouble.java
+++ java/src/jcowgill/prac16/sets/LinkedSetDouble.java
@@ -5,34 +5,34 @@ package jcowgill.prac16.sets;
  */
 public class LinkedSetDouble extends AbstractSetDouble
 {
-	private NodeDouble head;
-	private int size;
+    private NodeDouble head;
+    private int size;
 
-	/**
-	 * Creates an empty LinkedSetDouble
-	 */
-	public LinkedSetDouble()
-	{
-	}
+    /**
+     * Creates an empty LinkedSetDouble
+     */
+    public LinkedSetDouble()
+    {
+    }
 
-	@Override
-	public boolean add(Double d)
-	{
-	    // Verify this double does not exist in the list
-	    if (contains(d))
-	        return false;
+    @Override
+    public boolean add(Double d)
+    {
+        // Verify this double does not exist in the list
+        if (contains(d))
+            return false;
 
-	    // Add to the start
-	    this.head = new NodeDouble(d, this.head);
-	    this.size++;
-	    return true;
-	}
+        // Add to the start
+        this.head = new NodeDouble(d, this.head);
+        this.size++;
+        return true;
+    }
 
-	@Override
-	public boolean remove(Double d)
-	{
+    @Override
+    public boolean remove(Double d)
+    {
         // Scan the list for d
-	    NodeDouble previous = null;
+        NodeDouble previous = null;
         NodeDouble current = this.head;
 
         while (current != null)
@@ -54,18 +54,18 @@ public class LinkedSetDouble extends AbstractSetDouble
         }
 
         return false;
-	}
+    }
 
-	@Override
-	public void clear()
-	{
-	    this.head = null;
-	    this.size = 0;
-	}
+    @Override
+    public void clear()
+    {
+        this.head = null;
+        this.size = 0;
+    }
 
-	@Override
-	public boolean contains(Double d)
-	{
+    @Override
+    public boolean contains(Double d)
+    {
         // Scan the list for d
         NodeDouble current = this.head;
 
@@ -78,11 +78,11 @@ public class LinkedSetDouble extends AbstractSetDouble
         }
 
         return false;
-	}
+    }
 
-	@Override
-	public int size()
-	{
-		return size;
-	}
+    @Override
+    public int size()
+    {
+        return size;
+    }
 }

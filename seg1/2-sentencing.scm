f3be8cb3f279292cb61d13a0890e01070d74e631
James Cowgill <james410@cowgill.org.uk>
2013-10-11 16:08:06 +0100

    [Seg 1] Modified sentencing to use transative verbs

--- seg1/2-sentencing.scm
+++ seg1/2-sentencing.scm
@@ -3,13 +3,9 @@
 ; List of words
 (define noun-list (list 'dog 'cat 'student 'professor 'book 'computer))
 (define verb-list (list 'ran 'ate 'slept 'drank 'exploded 'decomposed))
+(define trans-verb-list (list 'crushed 'destroyed 'ravaged 'shattered 'slayed 'assimilated))
 (define adjective-list (list 'red 'slow 'dead 'pungent 'over-paid 'drunk))
 (define adverb-list (list 'quickly 'slowly 'wickedly 'majestically))
-
-; Definition of a sentence
-(define (sentence)
-  (append (list 'the) (noun-phrase) (verb-phrase))
-)
 
 ; Picks a random element from a list
 (define (pick-random lst)
@@ -19,6 +15,7 @@
 ; These functions select a random element from the relevant lists
 (define (a-noun)      (pick-random noun-list))
 (define (a-verb)      (pick-random verb-list))
+(define (a-trans-verb)(pick-random trans-verb-list))
 (define (a-adjective) (pick-random adjective-list))
 (define (a-adverb)    (pick-random adverb-list))
 
@@ -31,13 +28,34 @@
 (define (noun-phrase)
   (either
    (lambda () (list (a-noun)))
-   (lambda () (cons (a-adjective) (noun-phrase))))
+   (lambda () (cons (a-adjective) (noun-phrase)))
+  )
+)
+
+(define (the-noun-phrase)
+  (cons 'the (noun-phrase))
 )
 
 (define (verb-phrase)
   (either
    (lambda () (list (a-verb)))
-   (lambda () (list (a-verb) (a-adverb))))
+   (lambda () (list (a-verb) (a-adverb)))
+  )
+)
+
+(define (trans-verb-phrase)
+  (either
+   (lambda () (list (a-trans-verb)))
+   (lambda () (list (a-trans-verb) (a-adverb)))
+  )
+)
+
+; Final sentence definition
+(define (sentence)
+  (either
+   (lambda () (append (the-noun-phrase) (verb-phrase)))
+   (lambda () (append (the-noun-phrase) (trans-verb-phrase) (the-noun-phrase)))
+  )
 )
 
 (display (sentence))

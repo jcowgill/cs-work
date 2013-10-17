481114d04091bba010b5c527c24e97f4330eee7b
James Cowgill <james410@cowgill.org.uk>
2013-10-17 12:04:42 +0100

    [Seg 1] Added comments and spacing to original caring code

--- seg1/3-caring.scm
+++ seg1/3-caring.scm
@@ -1,9 +1,13 @@
 #lang racket
+
+; Doctor entry point
 (define (visit-doctor name)
   (begin
     (print (list 'hello name))
     (print '(what seems to be the trouble?))
     (doctor-driver-loop name)))
+
+; Main loop where commands are received and stuff is printed
 (define (doctor-driver-loop name)
   (begin
     (newline)
@@ -18,22 +22,32 @@
                     (print (reply user-response))
                     (doctor-driver-loop name)
                 )))))
+
+; Calculates the reply to a user's request
 (define (reply user-response)
   (if (fifty-fifty)
         (append (qualifier) (change-person user-response))
         (hedge)))
+
+; Returns true / false randomly
 (define (fifty-fifty) (= (random 2) 0))
+
+; Returns a random sentence qualifier
 (define (qualifier)
   (pick-random '((you seem to think)
                  (you feel that)
                  (why do you believe)
                  (why do you say))))
+
+; Returns a random hedging statement
 (define (hedge)
   (pick-random
    '((please go on)
      (many people have the same sorts of feelings)
      (many of my patients have told me the same thing)
      (please continue))))
+
+; Replaces all occurences of pattern with replacement in th given list
 (define (replace pattern replacement lst)
   (cond ((null? lst) '())
         ((equal? (car lst) pattern)
@@ -43,6 +57,10 @@
         (else (cons (car lst)
                     (replace pattern replacement (cdr lst)))
         )))
+
+; Performs many replacements within a list
+;  replacement-pairs = a list of pairs (pattern replacement)
+;   containing everything to replace and what to replace with
 (define (many-replace replacement-pairs lst)
   (if (null? replacement-pairs)
          lst
@@ -51,7 +69,11 @@
                          (cadr pat-rep)
                          (many-replace (cdr replacement-pairs)
                                        lst)))))
+
+; Changes the person in a phrase
 (define (change-person phrase)
   (many-replace '((i you) (me you) (am are) (my your))
                 phrase))
+
+; Chooses a random item from a list
 (define (pick-random lst) (list-ref lst (random (length lst))))

002efefa99514b5567057fe0222299e927d83ee2
James Cowgill <james410@cowgill.org.uk>
2014-02-12 20:50:04 +0000

    [SYAC Compilers] Move prac 3 lexer code to separate file

---
+++ syac/compilers/Prac3Lexer.hs
@@ -0,0 +1,44 @@
+-- Haskell Practical 3 Lexer for fuller prepositional calculus
+--  By James Cowgill
+
+module Prac3Lexer where
+
+import Data.Char
+
+-- Lexer tokens
+data Token =
+    TokTrue |
+    TokFalse |
+    TokNegate |
+    TokAnd |
+    TokOr |
+    TokImplies |
+    TokEqual |
+    TokLeft |
+    TokRight |
+    TokVar String
+    deriving (Eq, Show)
+
+-- Converts single characters into a token
+lexerSingle :: Char -> Token
+lexerSingle 'T'                = TokTrue
+lexerSingle 'F'                = TokFalse
+lexerSingle '-'                = TokNegate
+lexerSingle '*'                = TokAnd
+lexerSingle '+'                = TokOr
+lexerSingle '>'                = TokImplies
+lexerSingle '='                = TokEqual
+lexerSingle '('                = TokLeft
+lexerSingle ')'                = TokRight
+lexerSingle c                  = error ("lexical error: " ++ [c])
+
+-- Simple lexer for prepositional calculus
+lexer :: String -> [Token]
+lexer [] = []
+lexer (x:xs)
+    | isSpace x     = lexer xs
+    | isVariable    = TokVar vFst : lexer vSnd
+    | otherwise     = lexerSingle x : lexer xs
+    where
+        (vFst, vSnd)    = span (isAsciiLower) (x:xs)
+        isVariable      = not (null vFst)

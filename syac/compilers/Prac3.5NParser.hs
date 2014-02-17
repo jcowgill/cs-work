6c65a280b874d0b75d7a5ae281a60d654bed90e3
James Cowgill <james410@cowgill.org.uk>
2014-02-17 16:28:36 +0000

    [SYAC Compilers] Removed left-recursion from N1 grammar

--- syac/compilers/Prac3.5NParser.hs
+++ syac/compilers/Prac3.5NParser.hs
@@ -12,29 +12,144 @@ Original Grammar
     e   = e + e
         | e - e
         | e * e
-        | not e     (logical)
-        | e and e   (logical)
-        | e or e    (logical)
-        | e = e
-        | e < e
-        | true
-        | false
         | N
         | V
         | (e)
 
+    b   = not b     (logical)
+        | b and b   (logical)
+        | b or b    (logical)
+        | e = e
+        | e < e
+        | e > e
+        | (b)
+
     s   = skip
         | s; s
         | v := e
-        | if e then s else s
-        | while e s
+        | if b then s else s
+        | while b s
         | (s)
 
 Precedence + Associativity
     not     right
     *       left
     + -     left
-    <       left
+    < >     left
     =       left
     and     left
+    or      left
+
+Disambiguated
+    e   = e + e1
+        | e - e1
+        | e1
+    e1  = e1 * e2
+        | e2
+    e2  = N
+        | V
+        | (e)
+
+    b   = b or b1
+        | b1
+    b1  = b1 and b2
+        | b2
+    b2  = not b2
+        | b3
+    b3  = e = e
+        | e < e
+        | e > e
+        | (b)
+
+Left factored
+    e   = e eOp e1
+        | e1
+    eOp = + | -
+
+    b3  = e b3Op e
+        | (b)
+    b3Op= = | < | >
+
+Remove left recursion
+    e   = e1 e'
+    e'  = + e1 e'
+        | - e1 e'
+        | λ
+    e1  = e2 e1'
+    e1' = * e2 e1'
+        | λ
+    e2  = N
+        | V
+        | (e)
+
+    b   = b1 b'
+    b'  = or b1 b'
+        | λ
+    b1  = b2 b1'
+    b1' = and b2 b1'
+        | λ
+    b2  = not b2
+        | b3
+    b3  = e b3Op e
+        | (b)
+    b3Op= = | < | >
+
+    s   = s1 s'
+    s'  = ; s1 s'
+        | λ
+    s1  = skip
+        | v := e
+        | if b then s else s
+        | while b s
+        | (s)
+-}
+
+data NonTerminal = E | E' | E1 | E1' | E2 |
+                    B | B' | B1 | B1' | B2 | B3 | B3Op |
+                    S | S' | S1
+
+-- Main parse table
+parseTable :: NonTerminal -> Maybe Token -> [Symbol NonTerminal Token]
+parseTable E    _                   = [Nont E1, Nont E']
+parseTable E'   (Just TokSym '+')   = [Term '+', Nont E1, Nont E']
+parseTable E'   (Just TokSym '-')   = [Term '-', Nont E1, Nont E']
+parseTable E'   _                   = []
+parseTable E1   _                   = [Nont E2, Nont E1']
+parseTable E1'  (Just TokSym '*')   = [Term '*', Nont E2, Nont E1']
+parseTable E1'  _                   = []
+
+{-
+
+Remove left recursion
+    e   = e1 e'
+    e'  = + e1 e'
+        | - e1 e'
+        | λ
+    e1  = e2 e1'
+    e1' = * e2 e1'
+        | λ
+    e2  = N
+        | V
+        | (e)
+
+    b   = b1 b'
+    b'  = or b1 b'
+        | λ
+    b1  = b2 b1'
+    b1' = and b2 b1'
+        | λ
+    b2  = not b2
+        | b3
+    b3  = e b3Op e
+        | (b)
+    b3Op= = | < | >
+
+    s   = s1 s'
+    s'  = ; s1 s'
+        | λ
+    s1  = skip
+        | v := e
+        | if b then s else s
+        | while b s
+        | (s)
 -}

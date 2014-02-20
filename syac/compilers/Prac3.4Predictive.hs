cc37832c3726c0521ab6de51933ecb88473e5626
James Cowgill <james410@cowgill.org.uk>
2014-02-20 14:07:23 +0000

    [SYAC Compilers] Move generic ll parser into separate file

--- syac/compilers/Prac3.4Predictive.hs
+++ syac/compilers/Prac3.4Predictive.hs
@@ -2,21 +2,7 @@
 --  By James Cowgill
 
 import Prac3Lexer
-
--- Generic LL(1) Parser
---  Given a parsing table, starting symbol, list of tokens
---  Outputs true iff the list of tokens is in the language
-data Symbol n t = SymNont n | SymTerm t | SymError
-type Table n t = n -> Maybe t -> [Symbol n t]
-
-llparse :: Eq t => Table n t -> n -> [t] -> Bool
-llparse tb start = parseHelper [SymNont start]
- where
-  parseHelper []             []     = True
-  parseHelper (SymNont n:ss) []     = parseHelper ((tb n Nothing) ++ ss) []
-  parseHelper (SymNont n:ss) (c:cs) = parseHelper ((tb n (Just c)) ++ ss) (c:cs)
-  parseHelper (SymTerm t:ss) (c:cs) | t == c = parseHelper ss cs
-  parseHelper _              _      = False
+import Prac3LLParser
 
 {-
 First and follow sets

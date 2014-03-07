5e0c191e79d3a497711073d95809b974b2a12342
James Cowgill <james410@cowgill.org.uk>
2014-03-07 18:06:24 +0000

    [SYAC Compilers] Addec picture semantics and rendering

---
+++ syac/compilers/Prac5/Canvas.hs
@@ -0,0 +1,70 @@
+-- Haskell Practical 5 - Canvas
+--  By James Cowgill
+
+module Prac5.Canvas where
+
+import Data.List
+
+-- A canvas is a grid where each point can be black or white
+--  Stored as a list of black points
+--  They do not have fixed sizes, they can expand in any direction
+type Coords = (Int, Int)
+type Canvas = [Coords]
+
+-- Plus symbol
+plus :: Canvas
+plus = [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]
+
+-- Range function
+--  Generates a list of values from n to 0
+--  Returned list does not include the value n itself
+range :: Int -> [Int]
+range n | n < 0     = [n+1..0]
+        | otherwise = [0..n-1]
+
+-- Horizontal lines
+hor :: Coords -> Int -> Canvas
+hor (x, y) len = [(x + n, y) | n <- range len]
+
+-- Vertical lines
+ver :: Coords -> Int -> Canvas
+ver (x, y) len = [(x, y + n) | n <- range len]
+
+-- Overlay two canvases
+overlay :: Canvas -> Canvas -> Canvas
+overlay = union
+
+-- Canvas boundaries
+--  Returns left most and right most x coordinates
+xbounds :: Canvas -> (Int, Int)
+xbounds [] = (0, 0)
+xbounds c = (minimum xs, maximum xs)
+  where
+    xs = map fst c
+
+--  Returns top most and bottom most y coordinates
+ybounds :: Canvas -> (Int, Int)
+ybounds [] = (0, 0)
+ybounds c = (minimum ys, maximum ys)
+  where
+    ys = map snd c
+
+-- Returns the character to print for a cell
+cell :: Canvas -> Coords -> Char
+cell c coords | elem coords c = '#'
+              | otherwise     = ' '
+
+-- Renders a canvas
+renderCanvas :: Canvas -> String
+renderCanvas c = canvas top
+  where
+    canvas y | y > bottom = []
+             | otherwise  = row y left ++ canvas (y + 1)
+    row y x  | x > right  = "\n"
+             | otherwise  = cell c (x, y) : row y (x + 1)
+    (left, right) = xbounds c
+    (top, bottom) = ybounds c
+
+-- Prints a canvas to stdout
+printCanvas :: Canvas -> IO ()
+printCanvas = putStr . renderCanvas
---
+++ syac/compilers/Prac5/Picture.hs
@@ -0,0 +1,31 @@
+-- Haskell Practical 5 - Picture
+--  By James Cowgill
+
+module Prac5.Picture where
+
+import Data.List
+import Prac5.Canvas
+import Prac5.Lexer
+import Prac5.Parser
+
+-- 3 by 3 box
+box :: Picture
+box = [CmdHor (0, 0) 3,
+       CmdHor (2, 2) (-3),
+       CmdVer (0, 0) 3,
+       CmdVer (2, 0) 3]
+
+-- Count lines in picture
+count :: Picture -> Int
+count = length
+
+-- Read picture from string
+readPicture :: String -> Picture
+readPicture = doParse . alexScanTokens
+
+-- Draw picture to canvas
+drawPicture :: Picture -> Canvas
+drawPicture p = foldl' overlay [] (map drawCmd p)
+  where
+    drawCmd (CmdHor c l) = hor c l
+    drawCmd (CmdVer c l) = ver c l

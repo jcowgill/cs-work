26f7b7b2851b19b8ed0cc3ddc285762177663059
James Cowgill <james410@cowgill.org.uk>
2015-11-18 13:30:14 +0000

    [EMBS Open 1] Completed ex2 initial implementation

--- embs/open1/ex2/src/jcowgill/embs1/MoteSourceNode.java
+++ embs/open1/ex2/src/jcowgill/embs1/MoteSourceNode.java
@@ -1,26 +1,17 @@
 package jcowgill.embs1;
 
-import com.ibm.saguaro.logger.Logger;
 import com.ibm.saguaro.system.Assembly;
 import com.ibm.saguaro.system.DevCallback;
 import com.ibm.saguaro.system.Device;
 import com.ibm.saguaro.system.LED;
-import com.ibm.saguaro.system.Mote;
 import com.ibm.saguaro.system.Radio;
 import com.ibm.saguaro.system.SystemInfo;
 import com.ibm.saguaro.system.Time;
 import com.ibm.saguaro.system.Timer;
 import com.ibm.saguaro.system.TimerEvent;
-import com.ibm.saguaro.system.csr;
+import com.ibm.saguaro.system.Util;
 
-/**
- * A Source node implemented with MoteRunner
- *
- * LED Colours:
- * - Yellow = Turned on constantly after everything is initialized
- * - Green  =
- * - Red    =
- */
+/** A Source node implemented with MoteRunner */
 public final class MoteSourceNode
 {
 	/* LED Colours */
@@ -35,8 +26,11 @@ public final class MoteSourceNode
 	/** Number of channels */
 	private static final int CHANNELS = 3;
 
-	/** Number added to internal channels to get the external channel number */
-	private static final int EXT_CHANNEL_OFFSET = 11;
+	/** Number to add to the channel id to get the PAN id */
+	private static final int PAN_ID_OFFSET = 0x11;
+
+	/** The short address of this source node */
+	private static final int MY_SHORT_ADDRESS = 0x42;
 
 	/** The controller for the source node */
 	private static final SourceController controller;
@@ -46,6 +40,24 @@ public final class MoteSourceNode
 
 	/** Wakeup timer */
 	private static final Timer timer = new Timer();
+
+	/** Transmit buffer */
+	private static final byte[] xmit = new byte[12];
+
+	/** If true, we are currently receiving or transmitting */
+	private static boolean rxOn, txOn;
+
+	/**
+	 * Channel to send on first.
+	 *
+	 * If we try to send on a channel which we are not currently on (fairly
+	 * likely), this variable is filled with the channel number so we know what
+	 * it is after we've finished changing the channel.
+	 *
+	 * This variable is -1 if there is no pending send channel (ie ask the
+	 * controller for one instead).
+	 */
+	private static int pendingSendChannel = -1;
 
 	static
 	{
@@ -86,84 +98,20 @@ public final class MoteSourceNode
 			}
 		});
 
+		// Setup xmit fields which do not change
+		xmit[0] = Radio.FCF_DATA;
+		xmit[1] = Radio.FCA_SRC_SADDR | Radio.FCA_DST_SADDR;
+		Util.set16le(xmit, 9, MY_SHORT_ADDRESS);
+
 		// Initialize the source controller
 		controller = new SourceController(CHANNELS, Time.currentTime(Time.MILLISECS));
 
-		// Enter read mode
+		// Enter read mode and enable timer
+		radio.setShortAddr(MY_SHORT_ADDRESS);
+		handleControllerStateChange();
 
 		// Turn Yellow LED on once we've finished
 		LED.setState(LED_YELLOW, LED_ON);
-	}
-
-	/**
-	 * Attempts to change the channel of the radio
-	 *
-	 * If the radio receiver is enabled, it will request it to be turned off.
-	 *
-	 * @return true if the channel was changed immediately, false if it's pending
-	 */
-	private boolean trySetChannel(int channel)
-	{
-		// Calculate radio channel
-		byte radioChannel = (byte) (channel + EXT_CHANNEL_OFFSET);
-
-		// Don't do anything if the channel is correct
-		if (radio.getChannel() == radioChannel)
-			return true;
-
-		Logger.appendString(csr.s2b("trySetChannel Radio State = "));
-		Logger.appendInt(radio.getState());
-		Logger.flush(Mote.INFO);
-
-		switch (radio.getState())
-		{
-		case Device.S_ACTING:
-		case Device.S_RXSTP:
-		case Device.S_RXSTPTXING:
-			// Transmitting, do nothing until transmit has completed
-			return false;
-
-		case Device.S_ENABLING:
-		case Device.S_RXEN:
-		case Device.S_RXTXING:
-		case Device.S_RXTXPEND:
-			// Receiving, stop rx and wait
-			radio.stopRx();
-			return false;
-
-		default:
-			// Try to change it immediately in the other states
-			radio.setChannel(radioChannel);
-			return true;
-		}
-
-		/*
-		 *
-		 * Impossible:
-		 * S_CANCEL
-		 * S_CLOSED
-		 * S_FLAGS_MASK
-		 * S_IRQPEND
-		 * S_OFF
-		 * S_STATE_MASK
-		 * S_STDBY
-		 *
-		 * stopRx:
-		 * S_ENABLING		Read enabling
-		 * S_RXEN			Read is enabled
-		 * S_RXTXING		Transmitting, then read enabled
-		 * S_RXTXPEND		Reading, then transmitting, then read enabled
-		 *
-		 * wait:
-		 * S_ACTING			Transmitting, then OK
-		 * S_RXSTP			Read stopping
-		 * S_RXSTPTXING		Transmitting, then read stopping
-		 *
-		 * do now:
-		 * S_ACTIVE			OK
-		 * S_ACTIVATING		STDBY -> ACTIVE
-		 * S_WARMINGUP		OFF -> STDBY
-		 */
 	}
 
 	/** Assembly unload callback */
@@ -172,6 +120,7 @@ public final class MoteSourceNode
 		// Actions to perform when assembly is unloaded
 		if (type == Assembly.SYSEV_DELETED)
 		{
+			timer.cancelAlarm();
 			radio.close();
 			LED.setState(LED_YELLOW, LED_OFF);
 			LED.setState(LED_GREEN, LED_OFF);
@@ -184,21 +133,128 @@ public final class MoteSourceNode
 	/** Timer callback */
 	static void timerFire(byte param, long time)
 	{
-		// TODO Auto-generated method stub
-
+		// Send a wakeup event and handle any sends or channel changes
+		controller.wakeupEvent(Time.currentTime(Time.MILLISECS));
+		handleControllerStateChange();
 	}
 
 	/** Radio beacon received callback */
 	static int rxBeacon(int flags, byte[] data, int len, int info, long time)
 	{
-		// TODO Auto-generated method stub
+		// Handle new beacon packets
+		if (data != null)
+		{
+			// If we don't change the channel when sending, there is a small change we
+			//  will get a beacon for the wrong channel (which we will ignore).
+			if (controller.getReadChannel() != radio.getChannel())
+				return 0;
+
+			// Validate beacon
+			if (len != 12 || (data[0] & Radio.FCF_BEACON) == 0)
+				return 0;
+
+			// Notify controller
+			//  We send a wakeupEvent as well to ensure all pending sends are refreshed now
+			long absoluteTime = Time.currentTime(Time.MILLISECS);
+			int n = data[11];
+
+			controller.receiveBeacon(absoluteTime, n);
+			controller.wakeupEvent(absoluteTime);
+		}
+		else
+		{
+			// Receiving finished
+			rxOn = false;
+		}
+
+		// Handle any sends or channel changes
+		handleControllerStateChange();
 		return 0;
 	}
 
 	/** Radio transmit complete callback */
 	static int txComplete(int flags, byte[] data, int len, int info, long time)
 	{
-		// TODO Auto-generated method stub
+		txOn = false;
+		handleControllerStateChange();
 		return 0;
+	}
+
+	/** Handles any changes in the controller state (sends and channel hopping) */
+	private static void handleControllerStateChange()
+	{
+		// Reschedule timer event
+		timer.cancelAlarm();
+
+		long wakeupTime = controller.getNextWakeupTime();
+		if (wakeupTime > 0)
+			timer.setAlarmTime(wakeupTime);
+
+		// See if there is anything to transmit
+		int sendChannel;
+		if (pendingSendChannel != -1)
+		{
+			sendChannel = pendingSendChannel;
+			pendingSendChannel = -1;
+		}
+		else
+		{
+			sendChannel = controller.calcSendChannel();
+		}
+
+		if (sendChannel != -1)
+		{
+			// Switch channel and send
+			if (tryChangeChannel(sendChannel))
+			{
+				radio.transmit(Device.ASAP | Radio.TXMODE_POWER_MAX | Radio.TXMODE_CCA,
+						xmit, 0, xmit.length, 0);
+				txOn = true;
+			}
+			else
+			{
+				// Wait for an rxOff or txOff event
+				pendingSendChannel = sendChannel;
+			}
+		}
+		else if (tryChangeChannel(controller.getReadChannel()))
+		{
+			radio.startRx(Device.ASAP | Device.RX4EVER, 0, 0);
+			rxOn = true;
+		}
+	}
+
+	/**
+	 * Try and change the radio channel
+	 *
+	 * If the radio is on, it begins the turn off procedure.
+	 *
+	 * @param channel channel to change to
+	 * @return true if the channel was immediately changed
+	 */
+	private static boolean tryChangeChannel(int channel)
+	{
+		// Do nothing if we're already on the right channel
+		if (radio.getChannel() == channel)
+			return true;
+
+		// We can't do anything if the receiver or transmitter are on
+		if (rxOn)
+		{
+			radio.stopRx();
+			return false;
+		}
+
+		if (txOn)
+			return false;
+
+		// Change the channel
+		radio.setChannel((byte) channel);
+		radio.setPanId(PAN_ID_OFFSET + channel, true);
+
+		Util.set16le(xmit, 3, PAN_ID_OFFSET + channel); // Dest PAN
+		Util.set16le(xmit, 5, PAN_ID_OFFSET + channel); // Dest Address
+		Util.set16le(xmit, 7, PAN_ID_OFFSET + channel); // Source PAN
+		return true;
 	}
 }

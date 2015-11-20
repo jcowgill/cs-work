9cd5eaafe751dde0433b3b643d6c1a0de6b7574f
James Cowgill <james410@cowgill.org.uk>
2015-11-20 21:20:03 +0000

    [EMBS Open 1] Generalize the Sink class

--- embs/open1/ex2/src/embs/Sink.java
+++ embs/open1/ex2/src/embs/Sink.java
@@ -5,33 +5,41 @@ import com.ibm.saguaro.logger.*;
 
 public class Sink {
 
-	private static Timer  tsend;
-	private static Timer  tstart;
-	private static boolean light=false;
+	private static final int PAN_ID_OFFSET = 0x11;
 
-	private static byte[] xmit;
-	private static long	  wait;
-	static Radio radio = new Radio();
-	private static int n = 8; // number of beacons of sync phase - sample only, assessment will use unknown values
-	private static int nc;
+	private Timer  tsend;
+	private Timer  tstart;
+	private boolean light=false;
 
-	private static int t = 600; // milliseconds between beacons - sample only, assessment will use unknown values
+	private byte[] xmit;
+	private long	  wait;
+	private Radio radio = new Radio();
+	private int nc;
 
 	// settings for sink A
-	private static byte channel = 0; // channel 11
-	private static byte panid = 0x11;
-	private static byte address = 0x11;
+	private final int n;
+	private final byte channel;
 
-	static {
+	public Sink() {
+		// Default settings
+		this(0, 8, 600);
+	}
+
+	public Sink(int inChannel, int inN, int t) {
+		// Store arguments
+		this.channel = (byte) inChannel;
+		this.n = inN;
+
 		// Open the default radio
 		radio.open(Radio.DID, null, 0, 0);
 
 		// Set channel
-		radio.setChannel((byte)channel);
+		radio.setChannel(channel);
 
 		// Set the PAN ID and the short address
+		int panid = PAN_ID_OFFSET + channel;
 		radio.setPanId(panid, true);
-		radio.setShortAddr(address);
+		radio.setShortAddr(panid);
 
 		// Prepare beacon frame with source and destination addressing
 		xmit = new byte[12];
@@ -40,30 +48,33 @@ public class Sink {
 		Util.set16le(xmit, 3, panid); // destination PAN address
 		Util.set16le(xmit, 5, 0xFFFF); // broadcast address
 		Util.set16le(xmit, 7, panid); // own PAN address
-		Util.set16le(xmit, 9, address); // own short address
+		Util.set16le(xmit, 9, panid); // own short address
 
 		xmit[11] = (byte)n;
 
 		// register delegate for received frames
-		radio.setRxHandler(new DevCallback(null){
+		radio.setRxHandler(new DevCallback(this){
+				@Override
 				public int invoke (int flags, byte[] data, int len, int info, long time) {
-					return	Sink.onReceive(flags, data, len, info, time);
+					return ((Sink) obj).onReceive(flags, data, len, info, time);
 				}
 			});
 
 		// Setup a periodic timer callback for beacon transmissions
 		tsend = new Timer();
-		tsend.setCallback(new TimerEvent(null){
+		tsend.setCallback(new TimerEvent(this){
+				@Override
 				public void invoke(byte param, long time){
-					Sink.periodicSend(param, time);
+					((Sink) obj).periodicSend(param, time);
 				}
 			});
 
 		// Setup a periodic timer callback to restart the protocol
 		tstart = new Timer();
-		tstart.setCallback(new TimerEvent(null){
+		tstart.setCallback(new TimerEvent(this){
+				@Override
 				public void invoke(byte param, long time){
-					Sink.restart(param, time);
+					((Sink) obj).restart(param, time);
 				}
 			});
 
@@ -74,7 +85,7 @@ public class Sink {
 	}
 
 	// Called when a frame is received or at the end of a reception period
-	private static int onReceive (int flags, byte[] data, int len, int info, long time) {
+	int onReceive (int flags, byte[] data, int len, int info, long time) {
 		if (data == null) { // marks end of reception period
 			// turn green LED off
 			LED.setState((byte)1, (byte)0);
@@ -99,7 +110,7 @@ public class Sink {
 	}
 
 	// Called on a timer alarm
-	public static void periodicSend(byte param, long time) {
+	void periodicSend(byte param, long time) {
 		if(nc>0){
 			// transmit a beacon
 			radio.transmit(Device.ASAP|Radio.TXMODE_POWER_MAX, xmit, 0, 12, 0);
@@ -116,9 +127,8 @@ public class Sink {
 		}
 	}
 
-
 	// Called on a timer alarm, starts the protocol
-	public static void restart(byte param, long time) {
+	void restart(byte param, long time) {
 		nc=n;
 		xmit[11]=(byte)n;
 		tsend.setAlarmBySpan(0);
---
+++ embs/open1/ex2/src/embs/Sink13.java
@@ -0,0 +1,5 @@
+package embs;
+
+public class Sink13 {
+	public static Sink sink = new Sink(2, 10, 1500);
+}
